01ec4ef6d0d7379d03e781e9f9a52d489c58f81e
cheo2322 <36903744+cheo2322@users.noreply.github.com>
2018-03-09 16:27:00 -0500

    Update GraphNeuron.m

--- Perceptron/GraphNeuron.m
+++ Perceptron/GraphNeuron.m
@@ -1,7 +1,12 @@
-
+% -----------------------------------------------------------------------------
+% Author: Sergio Hidalgo
+% Tutor: Francisco Ortega, PhD
+% Plot the classification straightline
+% Inputs: W, a weights vector for plotting
+% ------------------------------------------------------------------------------
 
 function GraphNeuron(W)
-x = -1.5:0.01:1.5;
-y = (-W(1).*x+W(end))/W(2);
+x = -1.5:0.01:1.5;          % Plot in the range [-1,1] of 0.01 in 0.01
+y = (-W(1).*x+W(end))/W(2); % The equation used for plotting
 plot(x,y,'b');
 hold on;
