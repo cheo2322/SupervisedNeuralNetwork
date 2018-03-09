4a0534b5e73f2d6035530748c02a23e9d1dc8f92
cheo2322 <36903744+cheo2322@users.noreply.github.com>
2018-03-09 16:32:43 -0500

    Update and rename GraphNeuron.m to GraphPerceptron.m

--- Perceptron/GraphPerceptron.m
+++ Perceptron/GraphPerceptron.m
@@ -5,7 +5,7 @@
 % Inputs: W, a weights vector for plotting
 % ------------------------------------------------------------------------------
 
-function GraphNeuron(W)
+function GraphPerceptron(W)
 x = -1.5:0.01:1.5;          % Plot in the range [-1,1] of 0.01 in 0.01
 y = (-W(1).*x+W(end))/W(2); % The equation used for plotting
 plot(x,y,'b');
