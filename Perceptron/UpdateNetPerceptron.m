e228bf3d7515b7678e721950ca5bddfa9da3e2a4
cheo2322 <36903744+cheo2322@users.noreply.github.com>
2018-03-09 16:20:04 -0500

    Update UpdateNetPerceptron.m

--- Perceptron/UpdateNetPerceptron.m
+++ Perceptron/UpdateNetPerceptron.m
@@ -1,4 +1,16 @@
+% --------------------------------------------------------------------------------------
+% Author: Sergio Hidalgo
+% Tutor: Francisco Ortega, PhD
+% Update the weights, adding the new weights to 'EvolW'
+% Inputs: EvolW, the evolution weight matrix
+%         LR, the learning rate
+%         Output, the calculated output for the current pattern
+%         Target, the expected result for the curent pattern
+%         Input, the inputs vector for the current pattern
+% Outputs: EvolW, the evoltuion weights matrix, with a new column (vector weight) added
+% --------------------------------------------------------------------------------------
+
 function EvolW = UpdateNet(EvolW,LR,Output,Target,Input)
-New = EvolW(:,end)+(LR*(Target-Output)*Input)';
+New = EvolW(:,end)+(LR*(Target-Output)*Input)'; % 
 EvolW = [EvolW New];
 end
