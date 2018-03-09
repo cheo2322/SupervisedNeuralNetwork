dd3418ef8d54586c54189618664d1bca8ec58d27
cheo2322 <36903744+cheo2322@users.noreply.github.com>
2018-03-09 15:47:34 -0500

    Update CheckPatternPerceptron.m

--- Perceptron/CheckPatternPerceptron.m
+++ Perceptron/CheckPatternPerceptron.m
@@ -1,15 +1,12 @@
-function check = CheckPattern(Data,EvolW)
-flag = 0;
-for i=1:size(Data,1)
-    [~,Output,Target]=ValoresIOT(Data,EvolW,i);
-    if Output==Target
-        flag = flag+1;
-    end
-end
+----% ------------------------------------------------------------------------
+% Author: Sergio Hidalgo
+% Tutor: Francisco Ortega, PhD
+% Check if ou perceptron classify all patterns correctly
+% Inputs: Data, the input data (AND, OR tables)
+% Outputs: 1 o 0, 1 if all outputs are equal to targets, 0 in the other cases
+% ------------------------------------------------------------------------------
 
-if flag==size(Data,1)
-    check = 1;
-else
-    check = 0;
-end
+function check = CheckPatternPerceptron(Data,EvolW)
+Ones = ones(size(Data,1),1)*-1;                                     % Create a -1's column vector to add to Data
+check = sum([Data(:,1:end-1) Ones]*EvolW(:,end)-Data(:,end))==0;    % If there is not difference between our outputs and targets, all patterns are well-classified
 end
